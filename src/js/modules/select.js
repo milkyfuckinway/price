065b4dab0ec8354687f31cfa751a19a269ac9bb1
milkyfuckinway <0-0_0@bk.ru>
2023-01-07 07:34:28 +0300

    fix

--- src/js/modules/select.js
+++ src/js/modules/select.js
@@ -1,12 +1,13 @@
 import { watchBrands } from './brands.js';
 
 const select = document.querySelector('.select');
+const selectOptions = select.querySelector('.select__options');
 const selectSelected = select.querySelector('.select__selected');
 const selectResult = select.querySelector('.select__result');
 const selectList = select.querySelector('.select__list');
 const selectInput = select.querySelector('.select__input');
 const selectCheckboxes = select.querySelector('.select__checkboxes');
-
+const allCheckboxes = selectCheckboxes.querySelectorAll('input[type=checkbox]');
 const chronograph = select.querySelector('.select__chronograph');
 const argentum = select.querySelector('.select__silver');
 const gold = select.querySelector('.select__gold');
@@ -30,7 +31,7 @@ const checkParameter = () => {
   }
 };
 
-const createWatchList = () => {
+(function createWatchList() {
   for (let i = 0; i < Object.keys(watchBrands).length; i++) {
     const listItem = document.createElement('li');
     listItem.classList.add('select__item');
@@ -38,19 +39,15 @@ const createWatchList = () => {
     listItem.textContent = Object.values(watchBrands)[i].name;
     selectList.appendChild(listItem);
   }
-};
+})();
 
-createWatchList();
-
-const allCheckboxes = selectCheckboxes.querySelectorAll('input[type=checkbox]');
-
-const initialState = () => {
+(function initialState() {
   selectInput.value = '';
   allCheckboxes.forEach((item) => {
     item.disabled = true;
     item.checked = false;
   });
-};
+})();
 
 const setEnabled = () => {
   allCheckboxes.forEach((item) => {
@@ -58,36 +55,20 @@ const setEnabled = () => {
   });
 };
 
-initialState();
-
 const uncheck = () => {
   allCheckboxes.forEach((item) => {
     item.checked = false;
   });
 };
 
-select.addEventListener('click', (evt) => {
-  console.log(evt.target);
-  if (evt.target.closest('.select__options')) {
-    selectList.classList.remove('select__list--hidden');
-  }
-  if (evt.target.closest('.select__item')) {
-    selectList.classList.add('select__list--hidden');
-    uncheck();
-    selectInput.value = evt.target.id;
-    selectSelected.textContent = evt.target.textContent;
-    if (selectInput.value) {
-      setEnabled();
-    }
-    if (watchBrands[selectInput.value].canBePrecious) {
-      argentum.disabled = false;
-      gold.disabled = false;
-    } else {
-      argentum.disabled = true;
-      gold.disabled = true;
-    }
-  }
+selectOptions.addEventListener('click', () => {
+  selectList.classList.toggle('select__list--hidden');
+  selectList.addEventListener('click', onSelectMenuOpened);
+});
+
+selectCheckboxes.addEventListener('click', (evt) => {
   if (evt.target.closest('.select__checkboxes') && watchBrands[selectInput.value]) {
+    selectResult.textContent = checkParameter();
     if (watchBrands[selectInput.value].canBePrecious) {
       if (argentum.checked) {
         gold.disabled = true;
@@ -101,7 +82,31 @@ select.addEventListener('click', (evt) => {
       }
     }
   }
-  if (selectInput.value) {
-    selectResult.textContent = checkParameter();
-  }
 });
+
+function onSelectMenuOpened(evt) {
+  selectList.classList.add('select__list--hidden');
+  if (evt.target.closest('.select__item')) {
+    select.querySelectorAll('.select__item').forEach((item) => {
+      item.classList.remove('select__item--selected');
+    });
+    evt.target.classList.add('select__item--selected');
+    selectList.classList.add('select__list--hidden');
+    selectInput.value = evt.target.id;
+    selectSelected.textContent = evt.target.textContent;
+    if (selectInput.value) {
+      setEnabled();
+    }
+    if (watchBrands[selectInput.value].canBePrecious) {
+      argentum.disabled = false;
+      gold.disabled = false;
+    } else {
+      argentum.disabled = true;
+      gold.disabled = true;
+    }
+  }
+  selectResult.textContent = checkParameter();
+  selectList.classList.toggle('select__list--hidden');
+  selectList.removeEventListener('click', onSelectMenuOpened);
+  uncheck();
+}
